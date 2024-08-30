98172e013fa12f57f990db677a2cdf7295bd6fce
José Di Pietro <dijose29@gmail.com>
2024-08-29 23:58:28 -0400

    drag & drop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,19 @@ import Konva from "konva";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import { Stage, Layer, Image } from "react-konva";
-import baseImage from '../images/Mapa/panama base.svg';
-import captionsImage from '../images/Mapa/panama caption.svg';
-import geoImage from '../images/Mapa/panama geo.svg';
+import baseImage from "../images/Mapa/panama base.svg";
+import captionsImage from "../images/Mapa/panama caption.svg";
+import geoImage from "../images/Mapa/panama geo.svg";
 import useImage from "use-image";
 
 export default function Home() {
-  const [baseImage] = useImage('./Mapa/panama base.svg');
-  const [captionsImage] = useImage('./Mapa/panama caption.svg');
-  const [geoImage] = useImage('./Mapa/panama geo.svg');
-  const [ship] = useImage('./Iconos/Recurso 910.svg');
+  const [baseImage] = useImage("./Mapa/panama base.svg");
+  const [captionsImage] = useImage("./Mapa/panama caption.svg");
+  const [geoImage] = useImage("./Mapa/panama geo.svg");
+  const [ship] = useImage("./Iconos/Recurso 910.svg");
+
+  const [shipX, setShipX] = useState(460);
+  const [shipY, setShipY] = useState(280);
 
   console.log(window.innerWidth);
 
@@ -23,14 +26,41 @@ export default function Home() {
       // Rect and Circle are not DOM elements. They are 2d shapes on canvas */}
       <Stage width={1500} height={window.innerHeight}>
         <Layer>
-          <Image image={baseImage} alt="panama map" width={1200}  height={window.innerHeight} />
-          <Image x={460} y={280} image={ship} alt="panama map" width={80}  height={80} />
+          <Image
+            image={baseImage}
+            alt="panama map"
+            width={1200}
+            height={window.innerHeight}
+          />
         </Layer>
         <Layer>
-          <Image image={captionsImage} alt="captions" width={1200} height={window.innerHeight}/>
+          <Image
+            image={captionsImage}
+            alt="captions"
+            width={1200}
+            height={window.innerHeight}
+          />
         </Layer>
         <Layer>
-          <Image image={geoImage} alt="panama geo" width={1200} height={window.innerHeight} />
+          <Image
+            image={geoImage}
+            alt="panama geo"
+            width={1200}
+            height={window.innerHeight}
+          />
+          <Image
+            x={shipX}
+            y={shipY}
+            image={ship}
+            alt="panama map"
+            width={80}
+            height={80}
+            draggable
+            onDragEnd={(e) => {
+              setShipX(e.target.x());
+              setShipY(e.target.y());
+            }}
+          />
         </Layer>
       </Stage>
     </main>
