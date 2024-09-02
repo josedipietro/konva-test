fb74968754bee8e5db2f8ed93db705de6df884d2
José Di Pietro <dijose29@gmail.com>
2024-09-01 22:15:06 -0400

    layers buttons and export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,52 @@
-'use client';
-import dynamic from 'next/dynamic';
+"use client";
+import { Stage } from "konva/lib/Stage";
+import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
 
-const Map = dynamic(() => import('../components/Map'), {
+const Map = dynamic(() => import("../components/Map"), {
   ssr: false,
 });
- 
+
 export default function Home() {
+  const [mapActive, setMapActive] = useState(true);
+  const [geoActive, setGeoActive] = useState(true);
+  const [captionsActive, setCaptionsActive] = useState(true);
+  const [shipActive, setShipActive] = useState(true);
+
   return (
-    <main className="flex justify-center">
-      <Map />
+    <main className="flex justify-between">
+      <aside className="flex flex-col justify-center w-1/8 mx-2 px-16">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setMapActive(!mapActive)}
+        >
+          {mapActive ? "Desactivar" : "Activar"} mapa
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setCaptionsActive(!captionsActive)}
+        >
+          {captionsActive ? "Desactivar" : "Activar"} captions
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setGeoActive(!geoActive)}
+        >
+          {geoActive ? "Desactivar" : "Activar"} geo
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setShipActive(!shipActive)}
+        >
+          {shipActive ? "Desactivar" : "Activar"} barco
+        </button>
+      </aside>
+      <Map
+        mapActive={mapActive}
+        geoActive={geoActive}
+        captionsActive={captionsActive}
+        shipActive={shipActive}
+      />
     </main>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import jsPDF from "jspdf";
+import { Stage as KStage } from "konva/lib/Stage";
+import { MutableRefObject, RefObject, useRef, useState } from "react";
 import { Image, Layer, Stage } from "react-konva";
 import useImage from "use-image";
 
-export default function Map() {
+export default function Map({
+  mapActive,
+  captionsActive,
+  geoActive,
+  shipActive,
+}: {
+  mapActive: boolean;
+  geoActive: boolean;
+  captionsActive: boolean;
+  shipActive: boolean;
+  
+}) {
+  const mapRef = useRef<KStage>(null)
   const [baseImage] = useImage("./Mapa/panama base.svg");
   const [captionsImage] = useImage("./Mapa/panama caption.svg");
   const [geoImage] = useImage("./Mapa/panama geo.svg");
@@ -11,51 +25,82 @@ export default function Map() {
   const [shipX, setShipX] = useState(460);
   const [shipY, setShipY] = useState(280);
 
-  console.log(911)
+  const exportPdf = () => {
+    const stage = mapRef.current?.getStage();
+    if (!stage) {
+      console.log('data null');
+      return
+    }
+
+    var pdf = new jsPDF('l', 'px', [1400, 911]);
+    pdf.addImage(
+      stage.toDataURL({ pixelRatio: 2 }),
+      100,
+      0,
+      1400,
+      911
+    );
+
+    pdf.save('panamaMap.pdf');
+    /* const data = stage.toDataURL({ mimeType: 'image/jpeg', quality: 1 })
+
+    const link = document.createElement('a');
+    link.href = data;
+    link.download = 'map.jpg';
+    link.click(); */
+  }
   {
     /* // Stage - is a div wrapper
   // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
   // Rect and Circle are not DOM elements. They are 2d shapes on canvas */
   }
   return (
-    <Stage width={1500} height={911}>
-      <Layer>
-        <Image
-          image={baseImage}
-          alt="panama map"
-          width={1200}
-          height={911}
-        />
-      </Layer>
-      <Layer>
-        <Image
-          image={captionsImage}
-          alt="captions"
-          width={1200}
-          height={911}
-        />
-      </Layer>
-      <Layer>
-        <Image
-          image={geoImage}
-          alt="panama geo"
-          width={1200}
-          height={911}
-        />
-        <Image
-          x={shipX}
-          y={shipY}
-          image={ship}
-          alt="panama map"
-          width={80}
-          height={80}
-          draggable
-          onDragEnd={(e) => {
-            setShipX(e.target.x());
-            setShipY(e.target.y());
-          }}
-        />
-      </Layer>
-    </Stage>
+    <div className="flex">
+      <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4 mx-2 h-12"
+          onClick={exportPdf}
+        >
+          Exportar
+        </button>
+      <Stage width={1400} height={911} ref={mapRef}>
+        {mapActive ? (
+          <Layer>
+            <Image image={baseImage} alt="panama map" width={1200} height={911} />
+          </Layer>
+        ) : null}
+        {captionsActive ? (
+          <Layer>
+            <Image
+              image={captionsImage}
+              alt="captions"
+              width={1200}
+              height={911}
+            />
+          </Layer>
+        ) : null}
+        {geoActive ? (
+          <Layer>
+            <Image image={geoImage} alt="panama geo" width={1200} height={911} />
+          </Layer>
+        ) : null}
+        {shipActive ? (
+          <Layer>
+            <Image
+              x={shipX}
+              y={shipY}
+              image={ship}
+              alt="panama map"
+              width={80}
+              height={80}
+              draggable
+              onDragEnd={(e) => {
+                setShipX(e.target.x());
+                setShipY(e.target.y());
+              }}
+            />
+          </Layer>
+        ) : null}
+      </Stage>
+    </div>
   );
 }
