ac1aa70ad4a439f78c03b511125fb3fb8e8ec09c
José Di Pietro <dijose29@gmail.com>
2024-09-11 10:26:29 -0400

    Nuevo barco, capa decorativa y export imagen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   const [geoActive, setGeoActive] = useState(true);
   const [captionsActive, setCaptionsActive] = useState(true);
   const [shipActive, setShipActive] = useState(true);
+  const [ship2Active, setShip2Active] = useState(true);
+  const [aditionalActive, setAditionalActive] = useState(true);
 
   return (
     <main className="flex justify-between">
@@ -36,9 +38,21 @@ export default function Home() {
         </button>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setAditionalActive(!aditionalActive)}
+        >
+          {aditionalActive ? "Desactivar" : "Activar"} capa decorativa
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
           onClick={() => setShipActive(!shipActive)}
         >
           {shipActive ? "Desactivar" : "Activar"} barco
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+          onClick={() => setShip2Active(!ship2Active)}
+        >
+          {ship2Active ? "Desactivar" : "Activar"} barco 2
         </button>
       </aside>
       <Map
@@ -46,6 +60,8 @@ export default function Home() {
         geoActive={geoActive}
         captionsActive={captionsActive}
         shipActive={shipActive}
+        aditionalActive={aditionalActive}
+        ship2Active={ship2Active}
       />
     </main>
   );
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 import { Stage as KStage } from "konva/lib/Stage";
-import { MutableRefObject, RefObject, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Image, Layer, Stage } from "react-konva";
 import useImage from "use-image";
 
@@ -9,46 +9,63 @@ export default function Map({
   captionsActive,
   geoActive,
   shipActive,
+  aditionalActive,
+  ship2Active
 }: {
   mapActive: boolean;
   geoActive: boolean;
   captionsActive: boolean;
+  aditionalActive: boolean;
   shipActive: boolean;
-  
+  ship2Active: boolean;
 }) {
-  const mapRef = useRef<KStage>(null)
+  const mapRef = useRef<KStage>(null);
   const [baseImage] = useImage("./Mapa/panama base.svg");
   const [captionsImage] = useImage("./Mapa/panama caption.svg");
   const [geoImage] = useImage("./Mapa/panama geo.svg");
+  const [aditionalImage] = useImage("./Capa/1.svg");
   const [ship] = useImage("./Iconos/Recurso 910.svg");
+  const [ship2] = useImage("./Iconos/Recurso 810.svg");
 
   const [shipX, setShipX] = useState(460);
   const [shipY, setShipY] = useState(280);
 
+  const [ship2X, setShip2X] = useState(240);
+  const [ship2Y, setShip2Y] = useState(580);
+
   const exportPdf = () => {
     const stage = mapRef.current?.getStage();
     if (!stage) {
-      console.log('data null');
-      return
+      console.log("data null");
+      return;
     }
 
-    var pdf = new jsPDF('l', 'px', [1400, 911]);
-    pdf.addImage(
-      stage.toDataURL({ pixelRatio: 2 }),
-      100,
-      0,
-      1400,
-      911
-    );
+    var pdf = new jsPDF("l", "px", [1400, 911]);
+    pdf.addImage(stage.toDataURL({ pixelRatio: 2 }), 100, 0, 1400, 911);
 
-    pdf.save('panamaMap.pdf');
+    pdf.save("panamaMap.pdf");
     /* const data = stage.toDataURL({ mimeType: 'image/jpeg', quality: 1 })
 
     const link = document.createElement('a');
     link.href = data;
     link.download = 'map.jpg';
     link.click(); */
-  }
+  };
+
+  const exportImage = () => {
+    const stage = mapRef.current?.getStage();
+    if (!stage) {
+      console.log("data null");
+      return;
+    }
+
+    const data = stage.toDataURL({ mimeType: "image/jpeg", quality: 1 });
+
+    const link = document.createElement("a");
+    link.href = data;
+    link.download = "map.jpg";
+    link.click();
+  };
   {
     /* // Stage - is a div wrapper
   // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
@@ -56,16 +73,29 @@ export default function Map({
   }
   return (
     <div className="flex">
-      <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4 mx-2 h-12"
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded my-4 mx-2 h-14"
           onClick={exportPdf}
         >
-          Exportar
+          Exportar PDF
         </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded my-4 mx-2 h-14"
+          onClick={exportImage}
+        >
+          Exportar imagen
+        </button>
+      </div>
       <Stage width={1400} height={911} ref={mapRef}>
         {mapActive ? (
           <Layer>
-            <Image image={baseImage} alt="panama map" width={1200} height={911} />
+            <Image
+              image={baseImage}
+              alt="panama map"
+              width={1200}
+              height={911}
+            />
           </Layer>
         ) : null}
         {captionsActive ? (
@@ -80,7 +110,22 @@ export default function Map({
         ) : null}
         {geoActive ? (
           <Layer>
-            <Image image={geoImage} alt="panama geo" width={1200} height={911} />
+            <Image
+              image={geoImage}
+              alt="panama geo"
+              width={1200}
+              height={911}
+            />
+          </Layer>
+        ) : null}
+        {aditionalActive ? (
+          <Layer>
+            <Image
+              image={aditionalImage}
+              alt="panama geo"
+              width={1200}
+              height={911}
+            />
           </Layer>
         ) : null}
         {shipActive ? (
@@ -96,6 +141,23 @@ export default function Map({
               onDragEnd={(e) => {
                 setShipX(e.target.x());
                 setShipY(e.target.y());
+              }}
+            />
+          </Layer>
+        ) : null}
+        {ship2Active ? (
+          <Layer>
+            <Image
+              x={ship2X}
+              y={ship2Y}
+              image={ship2}
+              alt="panama map"
+              width={80}
+              height={80}
+              draggable
+              onDragEnd={(e) => {
+                setShip2X(e.target.x());
+                setShip2Y(e.target.y());
               }}
             />
           </Layer>
